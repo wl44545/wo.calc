b56a3ac34846daec799126c2bd3c51e381f026c8
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-12-22 14:55:35 +0100

    files

--- Calculator.py
+++ Calculator.py
@@ -5,8 +5,24 @@ class Calculator:
 	def __init__(self):
 		self.system = 'DEC'
 		self.word = 'Word'
-		self.value = None
+		self.current_value = None
 
+
+######## lukasz
+
+	def convert_system(self, value, input_system, output_system):
+		pass
+
+	def check_value_system(self, x): #czy liczba jest zgodna z systemem
+		pass
+
+
+########    monika
+	def check_value_word(self, x): #czy wartość mieści się w słowie
+		pass
+
+
+######## w poniedzialek
 	def insert_value(self, system, word, value):
 		if system == 'DEC':
 			converted_value = str(value)
@@ -21,3 +37,16 @@ class Calculator:
 			self.value = converted_value
 			return value
 
+
+
+	def dodawanie(self, a, b):
+		self.check_value_word(a)
+		self.check_value_system(a)
+		self.check_value_word(b)
+		self.check_value_system(b)
+		a1 = self.convert_system(a,self.system, 'Dec')
+		b1 = self.convert_system(a,self.system, 'Dec')
+		c = a + b
+		self.check_value_word(c)
+		self.check_value_system(c)
+		return c
